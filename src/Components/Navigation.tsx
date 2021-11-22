17d2a52a0c272798e9e4c23452c5a11850da66c2
Pragya Mukherjee <pragyamukherjee2017@gmail.com>
2021-11-22 10:17:42 +0530

    Used routing

--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Logo from '../Assets/Logo.png'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom"; //Use hashRouter for github pages to understand routes
 
 import { AppBar,Toolbar,Typography} from '@material-ui/core'
 
